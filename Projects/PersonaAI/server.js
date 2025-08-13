6d6f417d564c954cbedc23b3d7fd596186aea9f4
Syed Faizan Ali <syedfaizanali1450@gmail.com>
2025-08-14 05:03:39 +0530

    New Learnings

---
+++ Projects/PersonaAI/server.js
@@ -0,0 +1,41 @@
+const promptSync = require('prompt-sync')();
+const fs = require("fs");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const OpenAI = require("openai");
+const client = new OpenAI();
+
+const SYSTEM_PROMPT = fs.readFileSync("./systemPrompt.txt");;
+const PROFILE = fs.readFileSync("./profile.json");
+const chatHistory = fs.readFileSync("./_chat.txt");
+const currentTime = new Date().toLocaleTimeString();
+
+const prompt = SYSTEM_PROMPT + "\n\n" + "My Profile (Syed Faizan Ali)\n" + PROFILE + "\n\n" + "Chat History of me and shruti\n" + chatHistory + "\n\n" + "Currently Time is: " + currentTime;
+
+let messages = [{ "role": "system", "content": prompt }];
+
+const faizan = async (messages) => {
+    const response = await client.chat.completions.create({
+        model: "gpt-4.1-mini",
+        messages: messages,
+    });
+    return response.choices[0].message.content;
+}
+
+
+
+const persona = async () => {
+    while (true) {
+        let query = await promptSync('> ');
+        if(query.toLowerCase() == "stop") {
+            break;
+        }
+        messages.push({ "role": "user", "content": query });
+        response = await faizan(messages);
+        messages.push({ "role": "assistant", "content": response });
+        console.log('> ', response);
+    }
+}
+
+persona();
